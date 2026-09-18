0fa1fa225b3f299a4fe3a3faea83ae28267ced48
lukman582890-wq <lukman582890@gmail.com>
2026-09-18 20:41:34 +0700

    Bump service worker cache to v60

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='solar-amp-v59';
-const ASSETS=['./','./index.html','./styles.css?v=45','./app.js?v=59','./manifest.webmanifest','./t3k-wasm-module.js?v=59','./nam-default.nam'];
+const CACHE='solar-amp-v60';
+const ASSETS=['./','./index.html','./styles.css?v=60','./app.js?v=60','./manifest.webmanifest','./t3k-wasm-module.js?v=59','./nam-default.nam'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{
