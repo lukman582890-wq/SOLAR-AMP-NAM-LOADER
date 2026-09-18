e6eeb35a2f1c8ea6038ea9e71c51c88852b04984
lukman582890-wq <lukman582890@gmail.com>
2026-09-18 19:56:58 +0700

    Bump service worker cache to v51

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='solar-amp-v50';
-const ASSETS=['./','./index.html','./styles.css?v=50','./app.js?v=50','./manifest.webmanifest'];
+const CACHE='solar-amp-v51';
+const ASSETS=['./','./index.html','./styles.css?v=51','./app.js?v=51','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return r;}).catch(()=>caches.match(e.request))));
