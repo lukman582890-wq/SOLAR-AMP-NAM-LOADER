940b2382f991c7b3c6e6a5670c2e0c4cce75ab42
lukman582890-wq <lukman582890@gmail.com>
2026-09-18 18:41:36 +0700

    Bump cache for NAM URL loader fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='solar-amp-v35';
-const ASSETS=['./','./index.html','./styles.css?v=35','./app.js?v=35','./manifest.webmanifest'];
+const CACHE='solar-amp-v36';
+const ASSETS=['./','./index.html','./styles.css?v=36','./app.js?v=36','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return r;}).catch(()=>caches.match(e.request))));
