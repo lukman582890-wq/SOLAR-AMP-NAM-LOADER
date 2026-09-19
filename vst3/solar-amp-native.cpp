97b31bd257f8d6f21bc62cac6e97a5345c489cb4
lukman582890-wq <lukman582890@gmail.com>
2026-09-19 16:04:48 +0700

    Fix native AMP tone filter compile error

--- vst3/solar-amp-native.cpp
+++ vst3/solar-amp-native.cpp
@@ -204,8 +204,8 @@ void NeuralAmpModeler::ProcessBlock(iplug::sample** inputs, iplug::sample** outp
   if (!nativeAmpBypass && !namActive)
   {
     const double sr = sampleRate;
-    const lowAlpha = 1.0 - std::exp(-2.0 * 3.14159265358979323846 * 180.0 / sr);
-    const highAlpha = 1.0 - std::exp(-2.0 * 3.14159265358979323846 * 4200.0 / sr);
+    const double lowAlpha = 1.0 - std::exp(-2.0 * 3.14159265358979323846 * 180.0 / sr);
+    const double highAlpha = 1.0 - std::exp(-2.0 * 3.14159265358979323846 * 4200.0 / sr);
     const lowGain = std::pow(10.0, ((GetParam(kToneBass)->Value() - 5.0) * 2.4) / 20.0);
     const midGain = std::pow(10.0, ((GetParam(kToneMid)->Value() - 5.0) * 2.4) / 20.0);
     const highGain = std::pow(10.0, ((GetParam(kToneTreble)->Value() - 5.0) * 2.4) / 20.0);
